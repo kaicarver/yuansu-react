0e5af603d009524a3c25c5c14285bcb8958ff76d
Kai Carver <kaicarver@gmail.com>
2020-03-23 08:46:23 +0100

    check that all is well and dev can begin

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Welcome to yet another of Kai's web projects.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
