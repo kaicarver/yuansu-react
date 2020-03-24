dc8de416aa357b558690c3a43beb663f520e923f
Kai Carver <kaicarver@gmail.com>
2020-03-24 10:57:34 +0100

    a little more customized

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to yet another of Kai's web projects.
         </p>
         <HelloMessage name="Kai" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        Source: <a
+          className="App-link"
+          href="https://github.com/kaicarver/yuansu-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>. <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -22,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        </p>
       </header>
     </div>
   );
