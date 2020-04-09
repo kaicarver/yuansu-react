139a83d49aa031cde524738854adaa476451c0f7
Kai Carver <kaicarver@gmail.com>
2020-04-09 12:59:43 +0200

    pinyin on own line

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ function Detail(props) {
     <div className="chinese">
       <span>{el.trad} </span>
       <span>{el.simp !== el.trad ? el.simp : zhspace} </span>
-      <span className="pinyin">{hanzi.getPinyin(el.trad)[0]}</span>
+      <div className="pinyin">{hanzi.getPinyin(el.trad)[0]}</div>
     </div>
     <div className="english">{el.symbol}</div>
     <a href={el.source} target="_blank" rel="noopener noreferrer">{el.name}</a><br/>
