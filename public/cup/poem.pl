670fae8427ded38c9a4a9b653f7b0b6375b45679
Kai Carver <kaicarver@gmail.com>
2020-10-14 22:18:39 +0200

    rotated text

--- public/cup/poem.pl
+++ public/cup/poem.pl
@@ -1,9 +1,27 @@
 #!/usr/bin/perl
-# count words from blog
+
+# Rotate Chinese (or any) text,
+# from left-to-right, top-to-bottom reading order
+# to top-to-bottom, right-to-left order.
+#
+# For example,
+#
+#   ABCDE
+#   FGHIJ
+#   KLMNO
+#
+# becomes
+#
+#   KFA
+#   LGB
+#   MHC
+#   NID
+#   OJE
+#
+# Running the program four times returns the text to its original order.
 #
 # usage:
-#   perl countwords.pl ~/blog/days/2020-06-14.txt  # count for one day
-#   perl countpushups.pl 2020-06-1?.txt  # count for a bunch of days
+#   perl poem.pl poem.txt
 use 5.010;
 use strict;
 use warnings;
@@ -16,16 +34,30 @@ getopts("v:", \%options);
 
 my $verbose = defined $options{v} ? $options{v} : 0;
 
-# daily goals
-my $total_goal = 1000;
-my $total = 0;
-my $total_distinct = 0;
+my @chars;
+my $num_lines;
+my $num_chars = 0;
 
-my %count;
 while (my $line = <>) {
     chomp $line;
     #$line =~ s/(\p{CJK_Unified_Ideographs})/<$1>/g;
-    $line =~ s/(.)/<$1>/g;
-    say $line;
+    #$line =~ s/(.)/<$1>/g;
+    my @line = split "", $line;
+    push @chars, [@line];
+    if ($num_chars != scalar @line) {
+        if ($num_chars == 0) {
+            $num_chars = scalar @line;
+        } else {
+            warn "lines are not all same length";
+        }
+    }
+    #say join("*", @line);
 }
-
+$num_lines = scalar @chars;
+#say $chars[0][$num_chars-1];
+for my $i (0..$num_chars-1) {
+    for my $j (reverse(0..$num_lines-1)) {
+        print $chars[$j][$i];
+    }
+    print "\n";
+}
